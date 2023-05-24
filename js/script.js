6e7547cef8869c3e326db74164f164071fc7ef38
dariolaseta <dario.laseta@gmail.com>
2023-05-24 12:38:05 +0200

    added random number generator from jsnack-random

--- js/script.js
+++ js/script.js
@@ -49,3 +49,21 @@ function reset(){
 
     return selectedCells;
 }
+
+function randomNumberGenerator(min, max){
+    let numberArray = [];
+
+    if((max - min) < 4){
+        return [];
+    }
+
+    while(numberArray.length < 4){
+        let randomNumber = Math.floor(Math.random() * max) + min;
+        if(!numberArray.includes(randomNumber)){
+            numberArray.push(randomNumber);
+        }
+    }
+
+    console.log(numberArray);
+    return numberArray;
+}
